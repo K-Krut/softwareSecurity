d9c71a7570d99bfc6a34de86d485e215c245f0e9
K-Krut <katiakrut448@gmail.com>
2023-10-01 10:32:14 +0300

    logic of encryption changed

--- Project1/logic.py
+++ Project1/logic.py
@@ -2,50 +2,50 @@ from Project1.exeptions import ValidateKeyIntError, ValidateKeyLengthError, Vali
 
 
 class CaesarCipher:
-    def __init__(self, alphabet, key):
-        self.alphabet = alphabet
+    def __init__(self, english_alphabet, ukrainian_alphabet, key):
+        self.english_alphabet = english_alphabet
+        self.ukrainian_alphabet = ukrainian_alphabet
         self.key = key
 
-    def check_key(self, key):
-        if not isinstance(key, int):
-            raise ValidateKeyIntError(key)
-        if key < 0 or key >= len(self.alphabet):
+    def get_alphabet(self, char):
+        if char.upper() in self.english_alphabet:
+            return self.english_alphabet
+        elif char.upper() in self.ukrainian_alphabet:
+            return self.ukrainian_alphabet
+        else:
+            return None
+
+    def check_key(self, alphabet):
+        if not isinstance(self.key, int):
+            raise ValidateKeyIntError(self.key)
+        if self.key < 0 or self.key >= len(alphabet):
             raise ValidateKeyLengthError()
 
-    @staticmethod
-    def check_text(text):
+    def get_index(self, char, alphabet):
+        return (alphabet.index(char) + self.key) % len(alphabet)
+
+    def get_index_decrypt(self, char, alphabet):
+        return (alphabet.index(char) - self.key) % len(alphabet)
+
+    def get_symbol(self, char, is_upper, alphabet, operation):
+        self.check_key(alphabet)
+        index = self.get_index(char, alphabet) if operation == 'encrypt' else self.get_index_decrypt(char, alphabet)
+        return alphabet[index] if is_upper else alphabet[index].lower()
+
+    def process_text(self, text, operation='encrypt'):
+        result = ""
         for char in text:
-            if char not in text:
-                raise ValidateTextError(char)
-
-    def get_index(self, char):
-        return (self.alphabet.index(char) + self.key) % len(self.alphabet)
-
-    def get_symbol(self, char, is_upper):
-        index = self.get_index(char)
-        return self.alphabet[index] if is_upper else self.alphabet[index].lower()
+            is_upper = char.isupper()
+            char = char.upper()
+            alphabet = self.get_alphabet(char)
+            result += self.get_symbol(char, is_upper, alphabet, operation) if alphabet else char
+        return result
 
     def encrypt(self, text):
-        self.check_key(self.key)  # self.check_text(text)
-        encrypted_text = ""
-        for char in text:
-            is_upper = char.isupper()
-            char = char.upper()
-            encrypted_text += self.get_symbol(char, is_upper) if char in self.alphabet else char
-        return encrypted_text
+        return self.process_text(text, operation='encrypt')
 
     def decrypt(self, text):
-        self.check_key(self.key)
-        decrypted_text = ""
-        for char in text:
-            is_upper = char.isupper()
-            char = char.upper()
-            if char in self.alphabet:
-                index = (self.alphabet.index(char) - self.key) % len(self.alphabet)
-                decrypted_text += self.alphabet[index] if is_upper else self.alphabet[index].lower()
-            else:
-                decrypted_text += char
-        return decrypted_text
+        return self.process_text(text, operation='decrypt')
 
     def __str__(self):
-        return f'alphabet: {self.alphabet}'
+        return f'English Alphabet: {self.english_alphabet}\nUkrainian Alphabet: {self.ukrainian_alphabet}'
--- Project1/main.py
+++ Project1/main.py
@@ -2,8 +2,9 @@ from Project1.interface import CaesarCipherCLI
 from Project1.logic import CaesarCipher
 
 if __name__ == "__main__":
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГҐДЕЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ"
-    key = 1
-    cipher = CaesarCipher(alphabet, key)
+    english_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ukrainian_alphabet = "АБВГҐДЕЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ"
+    key = 4
+    cipher = CaesarCipher(english_alphabet, ukrainian_alphabet, key)
     cli = CaesarCipherCLI(cipher)
     cli.run()
