f038de737bbc4f2cea13d35aec23f1d84ce3a625
K-Krut <katiakrut448@gmail.com>
2023-10-07 19:23:15 +0300

    attack

--- Project2/main.py
+++ Project2/main.py
@@ -3,5 +3,6 @@ from Project2.logic import TritemiusCipher
 
 if __name__ == "__main__":
     cipher = TritemiusCipher('keyfortest')
-    cli = TritemiusCipherCLI(cipher)
-    cli.run()
+    # cipher = TritemiusCipher((2, 3))
+    # cipher = TritemiusCipher((2, 4, 6))
+    TritemiusCipherCLI(cipher).run()
